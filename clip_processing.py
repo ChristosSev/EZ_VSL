67a2894ad53572625309db546c7983570119685d
Christos Sevastopoulos <45820335+ChristosSev@users.noreply.github.com>
2022-07-22 22:25:34 -0500

    Update clip_processing.py

--- clip_processing.py
+++ clip_processing.py
@@ -3,50 +3,11 @@ import os
 import cv2
 import argparse
 from glob import glob
+import pathlib
 from pathlib import Path
+import math
 
-def extract_frames_and_audio(path):
-
-    print(path)
-    path_ss = path.replace(".mp4","")
-    path_s = path_ss.replace(".", "")
-    print(path_s)
-    y = Path(path).stem
-    # Read the video from specified path
-    cam = cv2.VideoCapture(path)
-    fps = cam.get(cv2.CAP_PROP_FPS)
-
-    if not os.path.exists(path_s):
-        os.makedirs(path_s + './frames/')
-        os.makedirs(path_s + './audio/')
-
-
-
-    currentframe = 0
-    while (True):
-    # reading from frame
-        ret, frame = cam.read()
-
-        if ret:
-            name = path_s + './frames/' + y + str(currentframe) + '.jpg'
-        # writing the extracted images
-            cv2.imwrite(name, frame)
-            currentframe += 1
-        else:
-            break
-
-
-    clip = mp.VideoFileClip(path)
-
-
-    clip.audio.write_audiofile(path_s + './audio/'+ y + r".wav")
-
-    cam.release()
-    cv2.destroyAllWindows()
-    path_sss = path_ss + './'
-    os.rename(path_sss,y)
-
-    return
+stem_list = []
 
 #### Parsing the arguments
 parser = argparse.ArgumentParser()
@@ -55,10 +16,77 @@ parser.add_argument('--path_to_clip',metavar='path_to_clip',  default='', type=s
 args= parser.parse_args()
 
 path = args.path_to_clip
+path_2 = args.path_to_clip
+
+def extract_frames_and_audio(path):
+
+    #print(path)
+    path_ss = path.replace(".mp4","")
+    #print(path_ss)
+    path_s = path_ss.replace(".", "")
+
+
+    path_final = os.path.dirname((path_s))
+    path_finale = path_final+ '/'
+   # print(path_finale)
+
+    y = Path(path).stem
+    # Read the video from specified path
+    cam = cv2.VideoCapture(path)
+    # fps = cam.get(cv2.CAP_PROP_FPS)
+
+
+    os.makedirs(path_finale + 'Testfiles',exist_ok=True)
+   # print("aaaaaaaa")
+    os.makedirs(path_finale + 'Testfiles/frames', exist_ok=True)
+    os.makedirs(path_finale + 'Testfiles/audio', exist_ok=True)
+        # os.makedirs(path_s + 'audio')
+
+    lastframe = 0
+    frame_list = []
+    frame_names = []
+    currentframe = 0
+
+    while (True):
+
+        ret, frame = cam.read()
+        frame_list.append(frame)
+
+        if ret:
+            name = path_finale + 'Testfiles/frames/' + y + str(currentframe) + '.jpg'
+            frame_names.append(name)
+            currentframe += 1
+        else:
+            break
+
+
+    center_frame = frame_list[math.ceil(len(frame_list)/2)]
+
+    center_frame_name = path_finale + 'Testfiles/frames/' + y + '.jpg'
+
+
+    cv2.imwrite(center_frame_name, center_frame)
+
+    clip = mp.VideoFileClip(path)
+    clip.audio.write_audiofile(path_finale + 'Testfiles/audio/'+ y + r".wav")
+
+    cam.release()
+    cv2.destroyAllWindows()
+    # path_sss = path_ss + './'
+    # os.rename(path_sss,y)
+    stem_list.append(y)
+   # print(stem_list)
+
+    return
 
 
 
 clips = glob(path + '*mp4') + glob(path + '*avi')
 for c in clips:
     extract_frames_and_audio(c)
+
+# for i in stem_list:
+
+
+
 
